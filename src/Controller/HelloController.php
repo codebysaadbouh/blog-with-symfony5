0c07f30587ff7e2e898c8140cbce715f864a386e
codebysaadbouh <74369527+codebysaadbouh@users.noreply.github.com>
2021-04-05 19:43:21 +0200

    Route annotation and Twig integration - Create First controller and route

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route; 
+
+class HelloController {
+
+    
+    /**
+     * @Route("/hello")
+     */
+    public function Hello() : Response{
+         return new Response("Hello World"); 
+    }
+
+    /**
+     * @Route("/hello/{name}")
+     */
+    public function HelloName($name) : Response {
+    return new Response('Hello '.$name); 
+    }
+
+}
