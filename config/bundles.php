b71ab769874c939e508a38df40fad7c558fa5e36
codebysaadbouh <74369527+codebysaadbouh@users.noreply.github.com>
2021-04-08 22:56:40 +0200

    Revert "Database configuration, finalization of models, creation of database and tables, Fixture, test Repository on HomeController.php"
    This reverts commit e2264c8c

--- config/bundles.php
+++ config/bundles.php
@@ -8,5 +8,4 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
